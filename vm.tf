59d5091f1e1e8f911b334f5c1873f171d2e58711
Akash2345ind <akash45678e@gmail.com>
2025-11-13 13:48:10 +0530

    vm-content

---
+++ vm.tf
@@ -0,0 +1,21 @@
+resource "google_compute_instance" "vm" {
+    name         = "vm"
+    zone         = "us-central1-a"
+    machine_type = "e2-medium"
+    boot_disk {
+        initialize_params {
+            image = "projects/ubuntu-os-cloud/global/images/ubuntu-2404-noble-amd64-v20250819"
+            size  = 20
+            type  = "pd-balanced"
+        }
+    }
+    metadata = {
+        enable-osconfig = "TRUE"
+        ssh-keys = "Akash:${file("/home/Akash/.ssh/id_rsa.pub")}"
+    }
+    network_interface {
+        access_config {
+        }
+        subnetwork = "default"
+    }
+}
