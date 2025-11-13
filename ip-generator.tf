622ad0a33dbfbe62e9196918d86f24d8d25e7c46
Akash2345ind <akash45678e@gmail.com>
2025-11-13 14:40:57 +0530

    correction

--- ip-generator.tf
+++ ip-generator.tf
@@ -1,4 +1,4 @@
 resource "local_file" "vm_ip" {
-  content = google_compute_instance.vm2.network_interface[0].access_config[0].nat_ip
+  content = google_compute_instance.vm.network_interface[0].access_config[0].nat_ip
   filename = "/home/Akash/vm_public_ip.txt"
 }
